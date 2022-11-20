e1f9034857a4f844fb437acec0153d1725a45989
Dylan Ramelli <rameld@usi.ch>
2022-11-20 12:51:58 +0100

    added some basic c++ for building

---
+++ c/core/io_tsp.hpp
@@ -0,0 +1,10 @@
+#ifndef IO_TSP_HPP
+#define IO_TSP_HPP
+
+class ProblemInstance{
+
+};
+
+
+
+#endif
---
+++ c/drivers/main.cpp
@@ -0,0 +1,11 @@
+#include "utils.hpp"
+#include "io_tsp.hpp"
+#include <vector>
+
+int main(int argv, const char *argc[]){
+
+    std::vector<int> solution[4];
+    std::vector<std::vector<int> > dist_matrix[4][4];
+
+    // compute_length();
+}
